300f6f1a9915ae27b490b47a1f40df8e244dc776
Hyunkwan Sung <gwansammy@gmail.com>
2024-06-12 14:38:55 +0900

    [Chore] 불필요한 테스트 모듈 제거

--- Timery/Modules/UI/SampleUIModule/Sources/ViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class SampleUIModuleViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        // Additional setup code here
-    }
-}
