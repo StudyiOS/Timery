8cb92082a80bd67155a6e31c3eabb54416aa9efb
Leo <brightspread.jo@gmail.com>
2024-06-16 17:34:58 +0900

    [Database-1] 불필요 코드 제거

--- Timery/Modules/Core/DatabaseManager/Sources/DatabaseManager/DatabaseManager.swift
+++ Timery/Modules/Core/DatabaseManager/Sources/DatabaseManager/DatabaseManager.swift
@@ -107,37 +107,3 @@ public final class DatabaseManager<E: DatabaseEntity>: DatabaseProtocol {
         return results.first
     }
 }
-
-
-//extension DatabaseManager: DatabaseProtocol {
-//    
-//    
-//    public func create<T: NSManagedObject>(_ item: T.Type, configure: (T) -> Void) throws {
-//        let entityName = String(describing: T.self)
-//        guard let entity = NSEntityDescription.entity(forEntityName: entityName, in: context) else {
-//            throw NSError(domain: "DatabaseManager", code: 1, userInfo: [NSLocalizedDescriptionKey: "Entity not found"])
-//        }
-//        let newItem = T(entity: entity, insertInto: context)
-//        configure(newItem)
-//        try context.save()
-//    }
-//    
-//    public func fetch<T: NSManagedObject>(_ item: T.Type, predicate: NSPredicate? = nil) throws -> [T] {
-//        let entityName = String(describing: T.self)
-//        let request = NSFetchRequest<T>(entityName: entityName)
-//        request.predicate = predicate
-//        
-//        return try context.fetch(request)
-//    }
-//    
-//    public func update<T: NSManagedObject>(_ item: T) throws {
-//        if context.hasChanges {
-//            try context.save()
-//        }
-//    }
-//    
-//    public func delete<T: NSManagedObject>(_ item: T) throws {
-//        context.delete(item)
-//        try context.save()
-//    }
-//}
